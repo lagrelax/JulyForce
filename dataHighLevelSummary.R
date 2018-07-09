725bcb26091b19d634b15de63d744bd6d08eec7a
Pengtuan Wu <lagrelax@gmail.com>
2018-07-08 22:59:01 -0400

    Updates of 20180708 Meeting

--- dataHighLevelSummary.R
+++ dataHighLevelSummary.R
@@ -2,43 +2,57 @@ library(Quandl)
 library(lubridate)
 library(dplyr)
 library(ggplot2)
+library(purrr)
+
+source('utilities.R')
 
 Quandl.api_key(Sys.getenv('QUANDL_API_KEY'))
 
+# Column description
+indicators <- Quandl.datatable('SHARADAR/INDICATORS')
+meta_indicators <- indicators %>% filter(table=='TICKERS')
+
 ticker_meta <- Quandl.datatable('SHARADAR/TICKERS',paginate=T)
 
-# Too many data to handle. Need to chunk the ticker and do multiple run. 
-# For now just use the first 1000 one.
-tmp_ticker <- ticker_meta$ticker[1:10]
+# Filter out Canadian tickers and other exchanges, 20k ticker left
+ticker_domestic <- ticker_meta %>% filter(category=='Domestic' & exchange %in% c('NYSE','NASDAQ'))
 
-# First date with records -- 1986-01-01
-min_date <- min(ticker_meta$firstpricedate)
+# Use first quarter column as first date of price/fundamental for now
+# Note some columns first quarter is NA, so have to do following treatment. Those tickers are NOT filtered
+tmp <- ticker_domestic %>% filter(!is.na(firstquarter))
+min_date <- min(tmp$firstquarter) %>% as.Date
 
-# Most recent quarter start date
-max_date <- as.Date('2018-07-01')
+# Most recent month end date
+max_date <- as.Date('2018-06-30')
 
-qtly_dates <- seq.Date(min_date,max_date,by='quarter')
+dates <- seq.Date(min_date,max_date,by='day') %>% adjust.previous(cal='NYSE') %>% unique
 
-# Get average volumn for each ticker during each quarter
-N <- length(qtly_dates)
+system.time(price_dt_all <- dates %>% map_df(~Quandl.datatable('SHARADAR/SEP',date=.x,paginate = T)))
 
-avg_volume_all <- NULL
-for (i in 2:N)
-{
-  start_date <- qtly_dates[i-1]
-  end_date <- qtly_dates[i]
-  
-  # Will add a multiple call here later to go thorugh all tickers
-  price_dt <- Quandl.datatable('SHARADAR/SEP',ticker=tmp_ticker,date.gte='2017-01-01',date.lte='2017-10-30',paginate = T)
-  
-  # Somehow still got zero data for some dates. Just skip 
-  if (nrow(price_dt)==0) next
-  
-  avg_volume <- price_dt %>% group_by(ticker) %>% summarise(avg_volume=mean(as.numeric(volume))) %>% mutate(start_date=start_date,end_date=end_date) %>% as.data.frame
-  
-  if(i>2) avg_volume_all <- rbind(avg_volume_all,avg_volume) else avg_volume_all <- avg_volume
-}
-
-avg_volume_all$end_date <- factor(avg_volume_all$end_date)
-
-ggplot(avg_volume_all)+geom_boxplot(aes(x=end_date,y=avg_volume)) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+# mthly_dates <- seq.Date(min_date+1,max_date+1,by='month')-1
+# mthly_dates <- adjust.previous(mthly_dates,cal='NYSE')
+# 
+# # Get average volumn for each ticker during each month
+# N <- length(mthly_dates)
+# 
+# tickers <- ticker_domestic$ticker
+# tmp <- chunk(tickers,getChunkSize(tickers))
+# 
+# avg_volume_all <- NULL
+# for (i in 2:N)
+# {
+#   start_date <- mthly_dates[i-1]
+#   end_date <- mthly_dates[i] 
+#   system.time(price_dt <- Quandl.datatable('SHARADAR/SEP',date.gte=start_date,date.lte=end_date,paginate = T))
+#   
+#   # Somehow still got zero data for some dates. Just skip 
+#   if (nrow(price_dt)==0) next
+#   
+#   avg_volume <- price_dt %>% group_by(ticker) %>% summarise(avg_volume=mean(as.numeric(volume))) %>% mutate(start_date=start_date,end_date=end_date) %>% as.data.frame
+#   
+#   if(i>2) avg_volume_all <- rbind(avg_volume_all,avg_volume) else avg_volume_all <- avg_volume
+# }
+# 
+# avg_volume_all$end_date <- factor(avg_volume_all$end_date)
+# 
+# ggplot(avg_volume_all)+geom_boxplot(aes(x=end_date,y=avg_volume)) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
---
+++ utilities.R
@@ -0,0 +1,19 @@
+library(timeDate)
+library(bizdays)
+
+# Get historical holiday dates for NYSE
+ny_holidays <- holidayNYSE(1992:2018)
+
+# Create a calendar exclude holidays and weekends
+create.calendar('NYSE',holidays = ny_holidays,weekdays = c('saturday','sunday'))
+
+# adjust.previous(max_date,'NYSE')
+
+# Chunk the list of tickers
+chunk <- function(x,n){
+  split(x,cut(seq_along(x),n,labels=F))
+}
+
+getChunkSize <- function(funds){
+  if(length(funds)<85) return(length(funds)) else return(ceiling(length(funds))/85)
+}
