8226d0ad0e02ed4c9536899ff91fdd794ad1d5c5
Pengtuan Wu <lagrelax@gmail.com>
2018-08-26 18:30:08 -0400

    first version of momentum calculation

---
+++ FactorLibrary_Momentum.R
@@ -0,0 +1,52 @@
+library(plyr)
+library(dplyr)
+library(TTR)
+library(purrr)
+
+load('data/filtered_price_dt_all.RData')
+
+# 21 day lag
+lag <- 21
+
+# look back window 504 days
+look_back <- 504
+
+# half life 126 days, (1-decay)^(halflife-1)=1/2
+half_life <- 126
+decay <- 1-0.5^(1/(half_life-1))
+
+log_price <- filtered_price_dt_all %>% select(ticker,date,closeunadj) %>% mutate(logP=log(closeunadj))
+
+# This is the data frame used for rolling momentum calculation
+log_rtn <- log_price %>% group_by(ticker) %>% arrange(rev(date)) %>% mutate(log1R=c(-diff(logP,1),NA))
+
+log_rtn <- log_rtn %>% arrange(ticker,date)
+biz_dates <- sort(unique(log_rtn$date))
+
+start_idx <- look_back+lag            
+result <- list()
+
+for(loop_idx in start_idx:length(biz_dates))
+{
+  loop_date <- biz_dates[loop_idx]
+  start_date <- biz_dates[loop_idx-look_back-lag+1]
+  end_date <- biz_dates[loop_idx-lag]
+  print(paste('Now calculating Momentum at',loop_date, 'Date range',start_date,'--', end_date))
+  rtn_df <- log_rtn %>% filter(date>=start_date&date<=end_date)
+  
+  # Check for each ticker, there rtn at the last date is not NA
+  rtn_check <- rtn_df %>% group_by(ticker) %>% summarise(max_date=max(date)) %>% left_join(rtn_df,by=c('ticker','max_date'='date'))
+  na_ticker <- rtn_check %>% filter(is.na(log1R))
+  print(paste('Filter out',nrow(na_ticker),'tickers'))
+  
+  filtered_tickers <- setdiff(unique(rtn_df$ticker),na_ticker$ticker)
+  
+  for(name in filtered_tickers)
+  {
+    print(paste('Calcuating ticker',name))
+    ticker_rtn_df <- rtn_df %>% filter(ticker==name)
+    ema <- EMA(ticker_rtn_df[,'log1R'],ratio=decay)
+    momentum <- ema[length(ema)]
+    result <- rbind(result,data.frame(ticker=name,date=loop_date,Momentum=momentum))
+  }
+}
