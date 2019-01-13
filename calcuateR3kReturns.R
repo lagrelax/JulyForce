e0ddc085196d1845c3512c65436c7c027bab579a
Pengtuan Wu <lagrelax@gmail.com>
2019-01-13 18:38:27 -0500

    Value portfolio

---
+++ calcuateR3kReturns.R
@@ -0,0 +1,18 @@
+options(stringsAsFactors = F)
+
+library(xts)
+library(PerformanceAnalytics)
+
+r3000 <- read.csv('Data/RUA3.csv')
+
+r3000$Date <- r3000$Date %>% as.Date
+
+r3000_xts <- as.xts(as.numeric(r3000$Adj.Close),order.by = r3000$Date)
+names(r3000_xts) <- 'R3000'
+
+r3000_rtn <- Return.calculate(r3000_xts,method='discrete')                    
+
+# Fill NA with 0
+r3000_rtn[is.na(r3000_rtn$R3000)] <- 0
+
+save(r3000_rtn,file='Data/R300DailyReturnXTS.RData')
