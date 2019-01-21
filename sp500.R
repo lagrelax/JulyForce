cdd7a2dc6f795c65bf49a73ea59d539dddaa2673
sven <sevenlinshiwen@gmail.com>
2019-01-21 18:37:46 -0500

    Update sp500.R
    adding backtesting

--- sp500.R
+++ sp500.R
@@ -7,7 +7,7 @@ library(xts)
 library(tidyr)
 library(PerformanceAnalytics)
 library(ggplot2)
-#library(plotly)
+library(plotly)
 
 source('utilities.R')
 
@@ -52,9 +52,108 @@ pb_dt_all <- fundamental_dt_all %>% select(ticker,pb,calendardate) %>% mutate(ye
 pb_monthly_mean_sd <- pb_dt_all %>% na.omit %>% group_by(yearmonth) %>% summarise(mean=mean(pb),sd=sd(pb))
 
 pb_z_score <- pb_dt_all %>% left_join(pb_monthly_mean_sd) %>% mutate(z_score=(pb-mean)/sd)
-pb_z_score <- pb_z_score %>% group_by(yearmonth) %>% mutate(z_rank=percent_rank(z_score)) %>% as.data.frame
 
 # sp500 all universe ## 
 sp500.all <- unique(univ$ticker)
 pb_z_score.sp500 <- pb_z_score %>% filter(ticker %in% sp500.all)
+
+pb_z_score.sp500 <- pb_z_score.sp500 %>% group_by(yearmonth) %>% mutate(z_rank=percent_rank(z_score)) %>% as.data.frame
 pb_z_score.sp500.sum <- pb_z_score.sp500 %>% group_by(yearmonth) %>% summarize(ticker_num=length(ticker))
+
+
+
+# backtesting 
+load('Data/All_daily_price.RData')
+
+value <- pb_z_score.sp500
+cut_off <- 0.025
+value_low <- value %>% filter(z_rank<=cut_off) %>% select(yearmonth,ticker,calendardate) %>% mutate(action='short')
+value_high <- value %>% filter(z_rank>=(1-cut_off)) %>%  select(yearmonth,ticker,calendardate) %>% mutate(action='long')
+
+#value_long_short <- value_high %>% rbind(value_low) %>% mutate(quarter=as.yearqtr(calendardate+1))
+
+value_long_short <- value_high %>% mutate(quarter=as.yearqtr(calendardate+1))
+
+securities <- unique(value_long_short$ticker)
+all_price <- price_dt_all %>% filter(ticker %in% securities)
+
+# Filter out penny stocks when their prices = 0
+all_price <- all_price %>% filter(close>0&volume>0)
+
+price_wide <- all_price %>% select(ticker,date,close) %>% mutate(close=as.numeric(close)) %>% spread(ticker,close)
+price_xts <- xts(price_wide[,-1],order.by = price_wide$date)
+
+rtn_xts <- Return.calculate(price_xts,method='discrete')
+
+# Fill the NA rtn with 0, when the stock close, treated as holding cash
+rtn_xts[is.na(rtn_xts)] <- 0
+
+rtn_df <- as.data.frame(rtn_xts)
+rtn_df$date <- rownames(rtn_df)
+rtn_df_long <- rtn_df %>% gather(ticker,return,-date) %>% mutate(date=as.Date(date)) 
+
+qtr_date <- unique(rtn_df[,'date',F]) %>% mutate(date=as.Date(date),quarter=as.yearqtr(date))
+qtr_date <- qtr_date %>% filter(quarter!='1998 Q4')
+
+rtn_df_long <- rtn_df_long %>% left_join(qtr_date) 
+
+rtn_qtly <- rtn_df_long %>% group_by(quarter,ticker) %>% summarise(return=prod(1+return))
+
+value_long_short <- value_long_short %>% filter(ticker %in% unique(rtn_qtly$ticker))
+
+value_sum <- value_long_short %>% group_by(quarter) %>% summarise(Nlong=sum(action=='long'),Nshort=sum(action=='short'))
+
+value_pos <- value_long_short %>% left_join(value_sum) %>% mutate(wgt=ifelse(action=='long',1/Nlong,-1/Nshort))
+
+# Filter out 1998
+value_pos <- value_pos %>% filter(calendardate>='1998-12-01')
+
+value_pos_rtn <- value_pos %>% left_join(rtn_qtly,by=c('ticker','quarter'))
+
+rtn_port <- value_pos_rtn %>% group_by(quarter) %>% summarise(Value=sum(wgt*return)-1)
+
+# Market BM
+load('Data/R300DailyReturnXTS.RData')
+
+# Convert daily R3000 return to qtly
+r3000_df <- r3000_rtn %>% as.data.frame
+r3000_df$date <- rownames(r3000_df) %>% as.Date
+rownames(r3000_df) <- NULL
+
+r3000_df <- r3000_df %>% left_join(qtr_date) %>% filter(!is.na(quarter))
+r3000_qtly_rtn <- r3000_df %>% group_by(quarter) %>% summarise(R3000=prod(1+R3000)-1)
+
+value_perf <- rtn_port %>% inner_join(r3000_qtly_rtn) 
+
+value_perf_long <- value_perf %>% gather(Port,Ret,-quarter)
+
+p <- ggplot(value_perf_long)+geom_line(aes(x=quarter,y=Ret,color=Port))
+ggplotly(p)
+
+tmp <- value_perf %>% mutate(date=as.Date(quarter)) 
+perf_xts <- as.xts(tmp[,c('Value','R3000')],order.by = tmp$date)
+
+
+# Avg Rtn (Qtly)
+N=nrow(perf_xts)/4
+apply(perf_xts,MARGIN = 2,mean)
+
+# Avg Rtn Annually
+port_rtn_annualized <- apply(perf_xts,MARGIN = 2,function(x)prod(1+x)^(1/N)-1)
+rtn_ann <- cbind(data.frame(Statistics='Annual Return'),t(port_rtn_annualized))
+
+# Sd Annually
+apply(perf_xts,MARGIN = 2,sd)*sqrt(4)
+sd_ann <- cbind(data.frame(Statistics='Annual Sd'),t(apply(perf_xts,MARGIN = 2,sd)*sqrt(4)))
+
+#Annualzied Sharp Ratio
+sharpe <- SharpeRatio.annualized(perf_xts,scale = 4)
+rownames(sharpe) <- NULL
+sharp_ann <- cbind(data.frame(Statistics='Annual Sharpe (Rf=0%)'),sharpe)
+
+# Summary Table
+summary <- rbind(rtn_ann,sd_ann,sharp_ann)
+summary
+charts.PerformanceSummary(perf_xts)
+write.csv(summary,file='Output/sp500_Value_Long_Short_0.025_Summary.csv',row.names = F)
+save(value_pos_rtn,perf_xts,summary,file='Data/sp500_Value_Long_Short_0.025.RData')
